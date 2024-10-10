7aedd6f34a684418a9484c54fca514e6df838ecf
George Ofogba <ofogbageorge3@gmail.com>
2024-10-10 18:02:30 +0100

    doctor controller updated

--- controllers/doctorController.js
+++ controllers/doctorController.js
@@ -1,4 +1,7 @@
 import doctorModel from '../models/doctorModel.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import appointmentModel from '../models/appointmentModel.js';
 
 const changeAvailability = async (req, res) => {
   try {
@@ -28,4 +31,167 @@ const getDoctorList = async (req, res) => {
   }
 };
 
-export { changeAvailability, getDoctorList };
+//api for doctor login
+const loginDoctor = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const doctor = await doctorModel.findOne({ email });
+    if (!doctor) {
+      return res.json({ success: false, message: 'Invalid Credentials' });
+    }
+    const isMatch = await bcrypt.compare(password, doctor.password);
+    if (isMatch) {
+      const token = jwt.sign({ id: doctor._id }, process.env.JWT_SECRET);
+
+      return res.json({
+        success: true,
+        message: 'Doctor fetched successfully',
+        token,
+      });
+    } else {
+      return res.json({ success: false, message: 'Invalid Credentials' });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to get all appointments
+const getDoctorAppointments = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
+    return res.json({
+      success: true,
+      message: 'Appointments fetched',
+      appointments,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to mark appointment completed
+const appointmentComplete = async (req, res) => {
+  try {
+    const { docId, appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    if (appointmentData && appointmentData.docId === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        isCompleted: true,
+      });
+      return res.json({ success: true, message: 'Appointment completed' });
+    } else {
+      return res.json({
+        success: false,
+        message: 'Cannot complete appointment',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to cancel appointment
+const appointmentCancel = async (req, res) => {
+  try {
+    const { docId, appointmentId } = req.body;
+    const appointmentData = await appointmentModel.findById(appointmentId);
+    if (appointmentData && appointmentData.docId === docId) {
+      await appointmentModel.findByIdAndUpdate(appointmentId, {
+        cancelled: true,
+      });
+      return res.json({ success: true, message: 'Appointment cancelled' });
+    } else {
+      return res.json({
+        success: false,
+        message: 'Cannot cancell appointment',
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to get doctor's dashboard data
+const doctorDashboard = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const appointments = await appointmentModel.find({ docId });
+
+    let earnings = 0;
+    appointments.map((item) => {
+      if (item.isCompleted || item.payment) {
+        earnings += item.amount;
+      }
+    });
+
+    let patients = [];
+
+    appointments.map((item) => {
+      if (!patients.includes(item.userId)) {
+        patients.push(item.userId);
+      }
+    });
+
+    const dashData = {
+      earnings,
+      appointments: appointments.length,
+      patients: patients.length,
+      latestAppointments: appointments.reverse().slice(0, 5),
+    };
+
+    return res.json({
+      success: true,
+      message: 'Dashboard data generated',
+      dashData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to get doctor's profile
+const doctorProfile = async (req, res) => {
+  try {
+    const { docId } = req.body;
+    const profileData = await doctorModel.findById(docId).select('-password');
+    return res.json({
+      success: true,
+      message: 'Doctor Profile fetched successfully',
+      profileData,
+    });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+//api to update doctor's profile
+const updateDoctorProfile = async (req, res) => {
+  try {
+    const { docId, fees, address, available } = req.body;
+    await doctorModel.findByIdAndUpdate(docId, { fees, address, available });
+    return res.json({ success: true, message: 'Doctor profile updated!' });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export {
+  changeAvailability,
+  getDoctorList,
+  loginDoctor,
+  getDoctorAppointments,
+  appointmentComplete,
+  appointmentCancel,
+  doctorDashboard,
+  doctorProfile,
+  updateDoctorProfile,
+};
